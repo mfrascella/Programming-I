552ef2640e72e7e7319e6707bde83b44e0535f8e
Maria Frascella <mfrascella16@gmail.com>
2018-01-31 19:25:49 -0500

    Initial commit

---
+++ ProjectMayflowerWeb/ProjectMayflowerWeb/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectMayflowerWeb.Models;
+using System.IO;
+
+namespace ProjectMayflowerWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Person> aListOfPeople = new List<Person>();
+            string[] names = System.IO.File.ReadAllLines(@"C:\Users\mfras\Downloads\Mayflower.csv");
+            int[] id = new int[names.Length];
+            for (int index = 1; index < names.Length; index++)
+            {
+
+                string[] tokens = names[index].Split(',');
+                int idn = Convert.ToInt32(tokens[0]);
+                string fname = tokens[1];
+                string lname = tokens[2];
+                string mInitial = tokens[3];
+                Person aPerson = new Person(idn, fname, lname, mInitial);
+
+                aListOfPeople.Add(aPerson);
+
+            }
+
+            int counter = 0;
+            string people = "";
+            while (counter < (names.Length - 1))
+            {
+                people += aListOfPeople[counter];
+                counter += 1;
+            }
+
+            ViewBag.Names = people;
+            return View();
+
+        }
+    }
+}
---
+++ ProjectMayflowerWeb/ProjectMayflowerWeb/Models/Person.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMayflowerWeb.Models
+{
+    public class Person
+    {
+        private string firstName = "n/a";
+        private string lastName = "n/a";
+        private string mInitial = "n/a";
+        private int id = -1;
+
+        // These are accessor methods aka Properties
+        // They are also called gets and sets or getters and setters
+
+        public string FirstName
+        {
+            //this gets info from the object
+            get
+            {
+                //this.variable points to variables at top
+                return this.firstName;
+            }
+            //this changes the object's value
+            set
+            {
+                    this.firstName = value;
+
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            //set
+            //{
+            //    this.id = value;
+            //}
+        }
+
+        public string MInitial
+        {
+            get
+            {
+                return this.mInitial;
+            }
+            set
+            {
+                this.mInitial = value;
+            }
+        }
+
+        //Start of constructors
+
+        //This is an empty constructor, replaces the invisible one
+        public Person() { }
+        public Person(int anId, string aLname, string aFname, string aMinitial)
+        {
+            this.id = anId;
+            this.FirstName = aFname;
+            this.LastName = aLname;
+            this.MInitial = aMinitial;
+        }
+
+        public override string ToString()
+        {
+            string aMessage = "";
+            aMessage += "ID: " + this.Id + " ";
+            aMessage += "First Name: " + this.FirstName + " ";
+            aMessage += "Middle Inital: " + this.MInitial + " ";
+            aMessage += "Last Name: " + this.LastName + " ";
+
+            return aMessage;
+        }
+    }
+}
