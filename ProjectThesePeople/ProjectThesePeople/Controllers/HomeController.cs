cde0da1a8d05cb434f8ca2832cbca2ae222ab88d
Maria Frascella <mfrascella16@gmail.com>
2018-01-31 19:18:20 -0500

    Initial commit

---
+++ ProjectThesePeople/ProjectThesePeople/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectThesePeople.Models;
+
+namespace ProjectThesePeople.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Maria";
+            person1.LastName = "Frascella";
+            person1.MiddleName = "Elizabeth";
+            //person1.Id = 16;
+
+            ViewBag.PersonList = person1;
+
+            return View();
+        }
+    }
+}
---
+++ ProjectThesePeople/ProjectThesePeople/Models/Person.cs
@@ -0,0 +1,79 @@
+﻿// Written by Maria Frascella
+// 1/23/18
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectThesePeople.Models
+{
+    public class Person
+    {
+        // private properties
+        private string firstName = "n/a";
+        private string lastName = "n/a";
+        private string middleName = "n/a";
+        private int id = -1;
+
+        // public gets and sets
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+
+        public string MiddleName
+        {
+            get
+            {
+                return this.middleName;
+            }
+            set
+            {
+                this.middleName = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            //set
+            //{
+            //    this.id = value;
+            //}
+        }
+
+        public override string ToString()
+        {
+            string message = "";
+            message += "First Name: " + this.FirstName + "<br />";
+            message += "Middle Name: " + this.MiddleName + "<br />";
+            message += "Last Name: " + this.LastName + "<br />";
+            message += "ID: " + this.Id + "<br /><br />";
+
+            return message;
+        }
+    }
+}
